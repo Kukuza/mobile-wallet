f72b66e6de17ccfcc95a3c549cb7b126ae0b3487
AlphonceMutebi <60338643+AlphonceMutebi@users.noreply.github.com>
2022-03-12 13:01:39 +0300

    testing github actions

--- __tests__/hello.spec.tsx
+++ __tests__/hello.spec.tsx
@@ -3,7 +3,7 @@ import { render } from "@testing-library/react-native";
 import Hello from "../Hello";
 
 describe("Hello", () => {
-  it("renders the correct message", () => {
+  it("renders the correct message testing github actions", () => {
     const { queryByText } = render(<Hello />);
     expect(queryByText("Hello, world!")).not.toBeNull();
   });
