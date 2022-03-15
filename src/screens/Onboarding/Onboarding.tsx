ef06ebdb7542d94b37662d13f8df76bb239515ac
AlphonceMutebi <60338643+AlphonceMutebi@users.noreply.github.com>
2022-03-15 15:31:41 +0300

    completed navigation of onboarding

--- src/screens/Onboarding/Onboarding.tsx
+++ src/screens/Onboarding/Onboarding.tsx
@@ -2,40 +2,60 @@ import { StyleSheet, Text, View, FlatList, Animated } from "react-native";
 import React, { useState, useRef } from "react";
 import slides from "../Onboarding/slides";
 import OnboardingItem from "../Onboarding/OnboardingItem";
-
-interface Props {
-  viewableItems: Number;
-}
+import Paginator from "./Paginator";
+import NextButton from "./NextButton";
 
 export default function Onboarding() {
   const scrollX = useRef(new Animated.Value(0)).current;
+  const slidesRef = React.useRef<any>(null);
   const [currentIndex, setCurrentIndex] = useState(0);
 
   const viewableItemsChanged = useRef(({ viewableItems }) => {
     setCurrentIndex(viewableItems[0].index);
   }).current;
+
+  const viewConfig = useRef({ viewAreaCoveragePercentThreshold: 50 }).current;
+
+  const scrollTo = () => {
+    if (currentIndex < slides.length - 1) {
+      slidesRef.current.scrollToIndex({ index: currentIndex + 1 });
+    } else {
+      console.log("Last item.");
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <FlatList
-        data={slides}
-        renderItem={({ item }) => (
-          <OnboardingItem
-            title={item.title}
-            id={item.id}
-            description={item.description}
-            image={item.image}
-          />
-        )}
-        keyExtractor={(item) => item.id}
-        horizontal
-        showsHorizontalScrollIndicator
-        pagingEnabled
-        bounces={false}
-        onScroll={Animated.event(
-          [{ nativeEvent: { contentOffset: { x: scrollX } } }],
-          { useNativeDriver: false }
-        )}
-        onViewableItemsChanged={viewableItemsChanged}
+      <View style={{ flex: 3 }}>
+        <FlatList
+          data={slides}
+          renderItem={({ item }) => (
+            <OnboardingItem
+              title={item.title}
+              id={item.id}
+              description={item.description}
+              image={item.image}
+            />
+          )}
+          keyExtractor={(item) => item.id}
+          horizontal
+          showsHorizontalScrollIndicator={false}
+          pagingEnabled
+          scrollEventThrottle={32}
+          bounces={false}
+          onScroll={Animated.event(
+            [{ nativeEvent: { contentOffset: { x: scrollX } } }],
+            { useNativeDriver: false }
+          )}
+          onViewableItemsChanged={viewableItemsChanged}
+          viewabilityConfig={viewConfig}
+          ref={slidesRef}
+        />
+      </View>
+      <Paginator data={slides} scrollX={scrollX} />
+      <NextButton
+        scrollTo={scrollTo}
+        percentage={(currentIndex + 1) * (100 / slides.length)}
       />
     </View>
   );
