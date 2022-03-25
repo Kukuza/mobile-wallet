d492c7ecc4326dd372becd4448527afca6b0b724
AlphonceMutebi <60338643+AlphonceMutebi@users.noreply.github.com>
2022-03-25 15:21:24 +0300

    minor changes

--- src/styles/colors/colors.ts
+++ src/styles/colors/colors.ts
@@ -37,37 +37,4 @@ export const COLORS = {
   wakalaModalLinearGradient: ["#F7EFFA", "#FCF8ED"],
 };
 
-// export const COLORS = {
-//   // base colors
-//   primary: "#4840BB",
-//   secondary: "#B0F623", // gray
-
-//   mainGray: "#B3B3B3",
-//   backgroundColor: "#E5E5E5",
-//   buttonGradient: ["rgba(183, 0, 76, 0.3)", "rgba(19, 63, 219, 1)"],
-
-//   //pdm
-//   inputFieldBorder: "rgba(195, 211, 212, 0.9)",
-//   inputFieldBackgroundColor: "rgba(242, 242, 242, 0.4)",
-//   inputFieldPlaceholder: "#9AA3AE",
-//   lightBlue: "#C0FAEB",
-//   warn: "#ba4848",
-
-//   // colors
-//   black: "#181725",
-//   textBlack: "#1C1939CC",
-//   white: "#FFFFFF",
-//   darkBlue: "#4840BB",
-//   tertiary: "#7C7C7C",
-//   realBlack: "#000000",
-//   buttonGray: "#E6E6E6",
-
-//   line: "#444444",
-//   lightGrey: "#F3EBFC",
-//   keypad: "#1C1939",
-
-//   //Menu
-//   homeMenu: "rgba(162, 197, 93, 0.15)",
-// };
-
 export default COLORS;
