3bfed3fca5218b264451791800125c6410b67fba
Allen-Muhani <allenmuhani@gmail.com>
2022-03-25 17:51:06 +0300

    Created top up mpayment request screen

---
+++ src/components/NavHeader.tsx
@@ -0,0 +1,67 @@
+import React, { useEffect, useState } from "react";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { useRoute, useNavigation } from "@react-navigation/native";
+import { Feather } from "@expo/vector-icons";
+import COLORS from '../styles/colors/colors';
+import { FONTS } from "../styles/fonts/fonts";
+
+function NavHeader(props) {
+  const route = useRoute();
+  const navigation = useNavigation();
+  const [title, setTitle] = useState(route.name);
+
+  useEffect(() => {
+    if (props.newTitle) {
+      setTitle(props.newTitle);
+    }
+  });
+
+  return (
+    <View style={styles.container}>
+      {!props.hideBackButton ? (
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => navigation.goBack()}
+        >
+          <Feather name="chevron-left" size={32} color={COLORS.primary} />
+        </TouchableOpacity>
+      ) : (
+        <View style={styles.dummyView} />
+      )}
+
+      {props.showTitle && <Text style={styles.title}>{title}</Text>}
+
+      <View style={styles.dummyView} />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    width: "100%",
+    height: 50,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+
+  button: {
+    width: 40,
+    height: 40,
+    marginLeft: 20,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+
+  title: {
+    ...FONTS.body3,
+    color: COLORS.black,
+    textAlign: "center",
+  },
+
+  dummyView: {
+    width: 40,
+  },
+});
+
+export default NavHeader;
---
+++ src/screens/TransactionRequests/TopUpViewRequestScreen.tsx
@@ -0,0 +1,110 @@
+import { Modal, StyleSheet, View } from "react-native";
+import React, { useState } from "react";
+import ScreenComponent from "../../containers/ScreenComponent";
+import { IStackScreenProps } from "../../navigation/StackScreenProps";
+import HeaderTitle from "../../components/HeaderTitle";
+import RequestTxInformationCard from "../../components/cards/RequestTxInformationCard";
+import LargeModal from "../../components/modals/LargeModals";
+import SwipeButton from "../../components/buttons/SwipeButton";
+import NavHeader from "../../components/NavHeader";
+
+/**
+ * 
+ * @param props { 
+ *          navigaion: 
+ *          route: 
+ * }
+ * @returns 
+ */
+const AddFundsConfirmationScreen: React.FunctionComponent<IStackScreenProps> = (props) => {
+  const { navigation, route } = props;
+
+  const [modalVisible, setModalVisible] = useState(false);
+
+  const [processIsError, setProcessIsError] = useState(false);
+
+  return (
+    <ScreenComponent>
+      <View style={styles.wrapper}>
+        {/* <HeaderTitle 
+            additionalStyling={styles.headerTitleAdditionalStyling}
+            backButtonHandler={ () => navigation.goBack() }
+            headerTxt={" Top Up Request"}
+            // title={" Top Up Request "}
+        /> */}
+
+        <NavHeader showTitle={true} newTitle={"Top Up Request"}></NavHeader>
+
+        <RequestTxInformationCard
+            additionalStyling={styles.requestTsxInfoCard}
+        ></RequestTxInformationCard>
+        
+        <SwipeButton 
+            title={"Swipe to Accept"}
+            handleAction={() => {
+                setModalVisible(true)
+            }}
+            additionalStyling={
+                styles.slidingButtonCustomStyling
+            }
+        />
+      </View>
+
+      <Modal
+          animationType="slide"
+          transparent={true}
+          visible={modalVisible}
+          onRequestClose={() => {
+            setModalVisible(!modalVisible);
+          }}
+          >
+
+        {!processIsError ? (
+            <View style={styles.modalOverlay}>
+                <LargeModal 
+                    onPressHanlder={() => setModalVisible(false)}
+                    title={"Oh Snap!"}
+                    message={"Something just happened. Please try  again."}
+                    btnText={"Try again"}
+                ></LargeModal>
+            </View>
+          ) : (
+            <View style={styles.modalOverlay}>
+              <LargeModal 
+                  onPressHanlder={() => setModalVisible(false)}
+                  title={"Money sent"}
+                  message={"cUSD 10 has been sent  to the Wakala escrow account. We shared your M-PESA number with the requesting member. We will notify you once the requesting member confirms the payment."}
+                  btnText={"Okay"}
+                  imageSrc={require("../../assets/images/modals/Shared_image.png")}
+              ></LargeModal>
+          </View>
+        )}
+      </Modal>
+    </ScreenComponent>
+  );
+};
+
+const styles = StyleSheet.create({
+  wrapper: {
+    flex: 1,
+    paddingHorizontal: 15,
+    paddingTop: 20
+  },
+  requestTsxInfoCard: {
+      minWidth: 375,
+      marginTop: 40
+  },
+  headerTitleAdditionalStyling: {
+      paddingLeft: 24
+  },
+  modalOverlay: {
+    flex: 1,
+    justifyContent: 'flex-end',
+  },
+  slidingButtonCustomStyling: {
+    alignSelf: 'center',
+    marginTop: 60
+  },
+});
+
+export default AddFundsConfirmationScreen;
