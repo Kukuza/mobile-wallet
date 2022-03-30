7a38fffbe5f00c512244fc65e39b47ed48eb5927
Mutinda-Kioko <66808040+Mutinda-Kioko@users.noreply.github.com>
2022-03-30 14:59:34 +0300

    Updated the colors file to match the dev branch

--- src/styles/colors/colors.ts
+++ src/styles/colors/colors.ts
@@ -1,13 +1,16 @@
 export const COLORS = {
   // Base
+  white: "#ffffff",
   primary: "#4840BB",
   accent1: "#133FDB",
   secondary: "#DB86C7",
+  shadowColor: "#7a5dba",
 
   // Alerts
   success: "#4840BB",
   warning: "#F3BB1C",
   error: "#EF8787",
+  warn: "#ba4848",
 
   // Gray Color variants
   grayLightest1: "#CFCFCF",
@@ -28,47 +31,29 @@ export const COLORS = {
   
 
   switchThumb:"#A2A3A2",
+
+  realBlack: "#000000",
+  black: "#181725",
+
   //
-  // buttonGradient: ["rgba(183, 0, 76, 0.3)", "rgba(19, 63, 219, 1)"],
+  buttonGradient: ["rgba(183, 0, 76, 0.3)", "rgba(19, 63, 219, 1)"],
+
+  cardGradient: [
+    "rgba(255, 140, 161, 0.08)",
+    "rgba(252, 207, 47, 0.08)",
+    "rgba(255, 255, 255, 0.08)",
+    "rgba(248, 48, 180, 0.08)",
+    "rgba(47, 68, 252, 0.08)",
+  ],
 
   // TODO Background color gradient
   gradientBackground: ["rgba(183, 0, 76, 0.3)", "rgba(19, 63, 219, 1)"],
+  menuBackground: "#F5F5F5",
 
   //TODO Other Gradients
   // gradientDark:["rgba(183, 0, 76, 0.3)", "rgba(19, 63, 219, 1)"]
+
+  wakalaModalLinearGradient: ["#F7EFFA", "#FCF8ED"],
 };
-
-// export const COLORS = {
-//   // base colors
-//   primary: "#4840BB",
-//   secondary: "#B0F623", // gray
-
-//   mainGray: "#B3B3B3",
-//   backgroundColor: "#E5E5E5",
-//   buttonGradient: ["rgba(183, 0, 76, 0.3)", "rgba(19, 63, 219, 1)"],
-
-//   //pdm
-//   inputFieldBorder: "rgba(195, 211, 212, 0.9)",
-//   inputFieldBackgroundColor: "rgba(242, 242, 242, 0.4)",
-//   inputFieldPlaceholder: "#9AA3AE",
-//   lightBlue: "#C0FAEB",
-//   warn: "#ba4848",
-
-//   // colors
-//   black: "#181725",
-//   textBlack: "#1C1939CC",
-//   white: "#FFFFFF",
-//   darkBlue: "#4840BB",
-//   tertiary: "#7C7C7C",
-//   realBlack: "#000000",
-//   buttonGray: "#E6E6E6",
-
-//   line: "#444444",
-//   lightGrey: "#F3EBFC",
-//   keypad: "#1C1939",
-
-//   //Menu
-//   homeMenu: "rgba(162, 197, 93, 0.15)",
-// };
 
 export default COLORS;
