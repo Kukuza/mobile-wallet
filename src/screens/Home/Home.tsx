bd25f036f77f2e3eb26a17daf9a2c5cfd25207de
AlphonceMutebi <60338643+AlphonceMutebi@users.noreply.github.com>
2022-04-19 16:40:29 +0300

    removed comments

--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -112,23 +112,6 @@ const HomeScreen: React.FunctionComponent<IStackScreenProps> = (props: any) => {
   );
 };
 
-// const mapStateToProps = (state) => {
-//   return {
-//     magic: state.magic,
-//     // transactions: state.transactions,
-//     // contractMethods: state.contractMethods,
-//   };
-// };
-// const mapDispatchToProps = (dispatch) => {
-//   return {
-//     dispatch: async (action) => {
-//       await dispatch(action);
-//     },
-//   };
-// };
-
-// export default connect(mapStateToProps, mapDispatchToProps)(HomeScreen);
-
 export default HomeScreen;
 
 const styles = StyleSheet.create({
