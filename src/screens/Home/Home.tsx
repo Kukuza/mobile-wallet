514e5d44881d1fa0d4d23ccd52938410d783f2c4
AlphonceMutebi <60338643+AlphonceMutebi@users.noreply.github.com>
2022-04-19 12:27:55 +0300

    minor changes

--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -19,6 +19,7 @@ import { EventData } from "web3-eth-contract";
 import { useState } from "react";
 import { WakalaEscrowTransaction } from "../../utils/Celo-Integration/transaction_types";
 import { magic } from "../../utils/magic";
+import { connect } from "react-redux";
 
 const EmptyList = (props) => {
   return (
@@ -111,7 +112,24 @@ const HomeScreen: React.FunctionComponent<IStackScreenProps> = (props: any) => {
   );
 };
 
-export default HomeScreen;
+const mapStateToProps = (state) => {
+  return {
+    magic: state.magic,
+    // transactions: state.transactions,
+    // contractMethods: state.contractMethods,
+  };
+};
+const mapDispatchToProps = (dispatch) => {
+  return {
+    dispatch: async (action) => {
+      await dispatch(action);
+    },
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(HomeScreen);
+
+// export default HomeScreen;
 
 const styles = StyleSheet.create({
   menu: {
