4af68c470b38b68e4da97558620c3de9050ffa4b
Daniel Mutinda <daniel.mutinda@dataoptions.co.ke>
2022-05-25 11:40:44 +0300

    Added the governance screen slides

---
+++ src/screens/Govenance/GovernanceItem.tsx
@@ -0,0 +1,64 @@
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  useWindowDimensions,
+  ImageSourcePropType,
+} from "react-native";
+import { COLORS } from "../../styles/colors/colors";
+import { FONTS } from "../../styles/fonts/fonts";
+interface Props {
+  id: string;
+  title: string;
+  description: string;
+  image: ImageSourcePropType;
+}
+
+const GovernanceItem: React.FC<Props> = ({ id, title, description, image }) => {
+  const { width } = useWindowDimensions();
+  return (
+    <View style={(styles.container, { width })}>
+      <View style={styles.image}>
+        <Image
+          source={image}
+          style={[{ width, resizeMode: "contain" }]}
+        />
+      </View>
+      <View style={{ flex: 0.3 }}>
+        <Text style={styles.header}>{title}</Text>
+        <Text style={styles.description}>{description}</Text>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+  },
+  image: {
+    flex: 0.6,
+    marginTop: 60,
+    justifyContent: "center",
+    alignItems: 'center'
+  },
+  header: {
+    ...FONTS.h3,
+    color: COLORS.primary,
+    fontWeight: "bold",
+    flex: 0.6,
+    textAlign: "center",
+  },
+  description: {
+    ...FONTS.body3,
+    color: COLORS.textColor2,
+    paddingHorizontal: 64,
+    flex: 1,
+    textAlign: "center",
+  },
+});
+
+export default GovernanceItem;
---
+++ src/screens/Govenance/slides.ts
@@ -0,0 +1,36 @@
+export default [
+  {
+    id: "1",
+    title: "Wakala DAO Governance",
+    description:
+      "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout",
+    image: require("../../assets/images/governance/g-slider-1.png"),
+  },
+  {
+    id: "2",
+    title: "Proposals",
+    description: "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout",
+    image: require("../../assets/images/governance/g-slider-2.png"),
+  },
+  {
+    id: "3",
+    title: "Voting",
+    description:
+      "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout",
+    image: require("../../assets/images/governance/g-slider-3.png"),
+  },
+  {
+    id: "4",
+    title: "Wakala Tokens",
+    description:
+      "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout",
+    image: require("../../assets/images/governance/g-slider-4.png"),
+  },
+  {
+    id: "5",
+    title: "Participate in Governance",
+    description:
+      "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout",
+    image: require("../../assets/images/governance/g-slider-5.png"),
+  }
+];
