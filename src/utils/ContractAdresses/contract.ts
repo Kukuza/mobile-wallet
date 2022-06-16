de86ecb3ef83368ec2f70b2481c3ca1fef5314a6
Allen <allenmuhani@gmail.com>
2022-06-16 15:34:09 +0300

    updated the smart contracts ABI and address

--- src/utils/ContractAdresses/contract.ts
+++ src/utils/ContractAdresses/contract.ts
@@ -1,6 +1,6 @@
 // Wakala Contract Address - Alfarojes
 export const WAKALA_CONTRACT_ADDRESS =
-  "0x1B342C833224d73d3fD65837Fd7abAA4094De3C6";
+  "0x46Ac9223D7390d86277AbdeeeafF29a426f017F5";
 
 // ERC20_ADDRESS - Alfarojes
 export const ERC20_ADDRESS = "0x874069Fa1Eb16D44d622F2e0Ca25eeA172369bC1";
