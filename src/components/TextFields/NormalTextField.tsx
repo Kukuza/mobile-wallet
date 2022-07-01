60ff242124f03e219d6d5cd53bfe0b775a0358e3
Mutinda-Kioko <kiokojohn2275@gmail.com>
2022-07-01 13:17:45 +0300

    Textfields update

--- src/components/TextFields/NormalTextField.tsx
+++ src/components/TextFields/NormalTextField.tsx
@@ -1,12 +1,44 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View,TextInput } from 'react-native'
+import React, {useState} from 'react'
+import { FONTS } from '../../styles/fonts/fonts'
+import COLORS from '../../styles/colors/colors'
 
-export default function NormalTextField() {
+export default function NormalTextField({inputLabel, onChangeText, placeholder, keyboardType}) {
+    const [onerrorInput, setOnErrorInput] = useState<Boolean>(true)
   return (
-    <View>
-      <Text>NormalTextField</Text>
-    </View>
+    <View style={[styles.inputWrapper, onerrorInput == true && styles.onErrorInput]}>
+    <Text style={styles.inputLabel}>{inputLabel}</Text>
+    <TextInput
+      style={styles.input}
+      onChangeText={onChangeText}
+      placeholder={placeholder}
+      keyboardType={keyboardType}
+      placeholderTextColor={COLORS.grayLightest1}
+      />
+  </View>
   )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    inputLabel: {
+        ...FONTS.s3,
+        marginTop: 5,
+        marginBottom: 1,
+        color: COLORS.textColor2
+      },
+    
+      input: {
+        paddingHorizontal: 2,
+        paddingVertical: 10,
+        ...FONTS.body4,
+    },
+    inputWrapper: {
+        backgroundColor:COLORS.white,
+        padding: 10,
+        borderRadius: 8
+      },
+    onErrorInput:{
+    borderWidth:1,
+    borderColor:COLORS.error
+      }
+})
