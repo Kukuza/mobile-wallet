9f4f28c81f257f171bd38f13c6229f85e3d07e4c
Mutinda-Kioko <kiokojohn2275@gmail.com>
2022-07-06 18:04:27 +0300

    Updating profile logic.....

--- src/screens/Settings/EditProfile.tsx
+++ src/screens/Settings/EditProfile.tsx
@@ -1,13 +1,48 @@
 import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import ScreenComponent from '../../containers/ScreenComponent'
 import { TouchableOpacity } from 'react-native-gesture-handler'
 import Blockie from '../../assets/icons/Blockie'
 import NormalTextField from '../../components/TextFields/NormalTextField'
 import { FONTS } from '../../styles/fonts/fonts'
 import COLORS from '../../styles/colors/colors'
+import { useDispatch, useSelector } from "react-redux";
+import { getProfile, INITIAL_STATE, saveProfile } from "../../store/Profile";
 
 export default function EditProfile({navigation}) {
+    const [profileName, setProfileName] = useState("")
+    const profile: IProfile = useSelector((state: any) => state.profile.data);
+    const dispatch = useDispatch();
+    useEffect(() => {
+        dispatch(getProfile());
+      }, []);
+
+      const continueHandler = () => {
+        let p: any;
+    
+        console.log("PROFILE", profile);
+          
+        if(profile) {
+          p = {
+            //insert the values to change
+            name: profileName ,
+            phoneNumber: profile.phoneNumber,
+            email: profile.email,
+            locale: profile.locale,
+            language: profile.language,
+            publicAddress: profile.publicAddress,
+            registered: profile.registered,
+            mnemonic: profile.mnemonic,
+            currencyCode: profile.currencyCode,
+            recoverySaved: profile.recoverySaved
+          }
+        }else {
+          p = INITIAL_STATE;
+          p.name = profileName
+        }
+    
+        dispatch(saveProfile(p));
+      }
   return (
     <ScreenComponent>
         <View style={styles.header}>
@@ -15,7 +50,7 @@ export default function EditProfile({navigation}) {
                 <Text style={styles.cancelText}>Cancel</Text>
             </TouchableOpacity>
             <Text style={styles.editText}>Edit Profile</Text>
-            <TouchableOpacity>
+            <TouchableOpacity onPress={() => continueHandler()}>
                 <Text style = {styles.saveText} >
                     Save
                 </Text>
@@ -27,7 +62,7 @@ export default function EditProfile({navigation}) {
       <View style={styles.textField}>
       <NormalTextField 
     inputLabel="Full name" 
-    onChangeText={null}
+    onChangeText={setProfileName}
     placeholder="Martin Chege"
     keyboardType="email-address"
     />
