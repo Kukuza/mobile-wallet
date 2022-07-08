97ef9c32b22f6fc1e3ca211a3ab8e633282cdb55
Daniel Mutinda <daniel.mutinda@dataoptions.co.ke>
2022-07-08 12:03:29 +0300

    Resolved routes

--- src/navigation/Routes.ts
+++ src/navigation/Routes.ts
@@ -44,7 +44,6 @@ import ViewAccountInfo from "../screens/Settings/AccountAddress/ViewAccountInfo"
 import RecoveryPhrase from "../screens/Settings/RecoveryPhrase/RecoveryPhrase";
 import ViewRecoveryInfo from "../screens/Settings/RecoveryPhrase/ViewRecoveryInfo";
 import RecoveryEnterPin from "../screens/Settings/RecoveryPhrase/RecoveryEnterPin";
-import RecoveryPhraseConfirm from "../screens/Onboarding/RecoveryPhraseConfirm";
 
 const routes: IRouteProps[] = [
   {
@@ -114,10 +113,6 @@ const routes: IRouteProps[] = [
   {
     name: "AccountAddress",
     component: AccountAddress,
-  },
-  {
-    name:"RecoveryPhraseConfirm",
-    component:RecoveryPhraseConfirm
   },
   {
     name:"EnterPin",
